b42fbca622d682736045e0b236bce977fbc909e5
Fadumo111 <116730846+Fadumo111@users.noreply.github.com>
2023-06-27 22:19:03 +0300

    inventory table

--- frontend/src/pages/Inventory.jsx
+++ frontend/src/pages/Inventory.jsx
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, HStack } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import { Input, Text, Box, VStack } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 
 function Inventory() {
   return (
@@ -11,14 +22,89 @@ function Inventory() {
         <VStack>
           <HStack>
             <Text as="b">Search:</Text>
-            <Input htmlSize={15} width="auto" />
+            <Input htmlSize={25} width="auto" />
           </HStack>
           <HStack>
             <Text as="b">Filter by category:</Text>
-            <Input htmlSize={15} width="auto" />
+            <Input htmlSize={25} width="auto" />
           </HStack>
         </VStack>
       </Box>
+      <Table  colorScheme='#D3D3D3' size='md'>
+      <Tr>
+        <Th>Name</Th>
+        <Th >Category</Th>
+        <Th isNumeric>Quantity</Th>
+        <Th isNumeric>Price</Th>
+        <Th isNumeric>Threshhold</Th>
+      </Tr>
+        <Tbody>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          {/* Additional rows */}
+        </Tbody>
+      </Table>
     </>
   );
 }
